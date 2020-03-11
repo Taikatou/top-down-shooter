d0ea692bd44a6ba6336cc9c06b8d6248030affe4
Taikatou <conorstephens@gmail.com>
2020-03-11 16:43:12 +0000

    update all

---
+++ Assets/Research/Scripts/DirectionsKeyMapper.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Research.Scripts;
+using UnityEngine;
+
+public class DirectionsKeyMapper : MonoBehaviour
+{
+    private Dictionary<Directions, KeyCode> _primaryDirections;
+
+    private Dictionary<Directions, KeyCode> _secondaryDirections;
+
+    private Dictionary<Vector2, Directions> _directionsVectorMap;
+
+    private Dictionary<Directions, Vector2> _vectorDirectionsMap;
+
+    private void Start()
+    {
+        _primaryDirections = new Dictionary<Directions, KeyCode>
+        {
+            {Directions.Left, KeyCode.A},
+            {Directions.Right, KeyCode.D },
+            {Directions.Down, KeyCode.S},
+            {Directions.Up, KeyCode.W }
+        };
+        _secondaryDirections = new Dictionary<Directions, KeyCode>
+        {
+            {Directions.Left, KeyCode.LeftArrow},
+            {Directions.Right, KeyCode.RightArrow },
+            {Directions.Down, KeyCode.DownArrow },
+            {Directions.Up, KeyCode.UpArrow }
+        };
+        _directionsVectorMap = new Dictionary<Vector2, Directions>
+        {
+            { new Vector2(-1, 0), Directions.Left },
+            { new Vector2(1, 0), Directions.Right },
+            { new Vector2(0, 1), Directions.Up },
+            { new Vector2(0, -1), Directions.Down },
+        };
+        _vectorDirectionsMap = new Dictionary<Directions, Vector2>
+        {
+            { Directions.Left, new Vector2(-1, 0)  },
+            { Directions.Right, new Vector2(1, 0) },
+            { Directions.Up,  new Vector2(0, 1)},
+            { Directions.Down, new Vector2(0, -1) },
+            { Directions.None, new Vector2(0, 0) }
+        };
+    }
+
+    public Directions GetDirectionVector(Vector2 input)
+    {
+        Debug.Log(input);
+        if (_directionsVectorMap.ContainsKey(input))
+        {
+            return _directionsVectorMap[input];
+        }
+        return Directions.None;
+    }
+
+    public Vector2 GetVectorDirection(Directions direction)
+    {
+        return _vectorDirectionsMap[direction];
+    }
+
+    public Vector2 GetVectorDirection(float direction)
+    {
+        return GetVectorDirection((Directions)direction);
+    }
+
+    private int GetInput(KeyCode negativeKey, KeyCode positiveKey)
+    {
+        var negative = Input.GetKey(negativeKey);
+        var positive = Input.GetKey(positiveKey);
+        if (negative ^ positive)
+        {
+            return negative ? 1 : 2;
+        }
+
+        return 0;
+    }
+
+    public Directions GetDirection(Dictionary<Directions, KeyCode> directions)
+    {
+        var x = GetInput(directions[Directions.Left], directions[Directions.Right]);
+        var y = GetInput(directions[Directions.Down], directions[Directions.Up]);
+
+        var input = new Vector2(x, y);
+        return GetDirectionVector(input);
+    }
+
+    public Directions PrimaryDirections => GetDirection(_primaryDirections);
+
+    public Directions SecondaryDirections => GetDirection(_secondaryDirections);
+}
--- Assets/Research/Scripts/TopDownAgent.cs
+++ Assets/Research/Scripts/TopDownAgent.cs
@@ -1,87 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MLAgents;
 using UnityEngine;
 
 namespace Research.Scripts
 {
+    public enum Directions { None, Left, Right, Up, Down }
+
     public class TopDownAgent : Agent
     {
         public TopDownInputManager inputManager;
 
-        private enum Directions { Left, Right, Up, Down }
+        public DirectionsKeyMapper directionsKeyMapper;
 
-        private Dictionary<Directions, KeyCode> _directions;
-
-        private Dictionary<Directions, KeyCode> _secondaryDirections;
-        public override void InitializeAgent()
+        public override void OnActionReceived(float[] vectorAction)
         {
-            base.InitializeAgent();
-
-            _directions = new Dictionary<Directions, KeyCode>()
-            {
-                {Directions.Left, KeyCode.A},
-                {Directions.Right, KeyCode.D },
-                {Directions.Down, KeyCode.S},
-                {Directions.Up, KeyCode.W }
-            };
-            _secondaryDirections = new Dictionary<Directions, KeyCode>()
-            {
-                {Directions.Left, KeyCode.LeftArrow},
-                {Directions.Right, KeyCode.RightArrow },
-                {Directions.Down, KeyCode.DownArrow },
-                {Directions.Up, KeyCode.UpArrow }
-            };
-        }
-
-        private int GetDecision(float input)
-        {
-            var output = 0;
-            switch (Mathf.FloorToInt(input))
-            {
-                case 1:
-                    // Left or Down
-                    output = -1;
-                    break;
-                case 2:
-                    // Right or Up
-                    output = 1;
-                    break;
-            }
-            return output;
-        }
-
-        public override void AgentAction(float[] vectorAction)
-        {
-
-            // inputManager.SetAIPrimaryMovement(xInput, yInput);
-        }
-
-        private int GetInput(KeyCode negativeKey, KeyCode positiveKey)
-        {
-            var negative = Input.GetKey(negativeKey);
-            var positive = Input.GetKey(positiveKey);
-            if (negative ^ positive)
-            {
-                return negative ? 1 : 2;
-            }
-
-            return 0;
+            var primaryDirection = directionsKeyMapper.GetVectorDirection(vectorAction[0]);
+            inputManager.SetAiPrimaryMovement(primaryDirection);
+            // var secondaryDirection = (Directions)vectorAction[0];
         }
 
         public override float[] Heuristic()
         {
-            var direction = 0.0f;
-
-
-            if (_directions != null)
+            Debug.Log("Heuristic");
+            var output = new []
             {
-                var x = GetInput(_directions[Directions.Left], _directions[Directions.Right]);
-                var y = GetInput(_directions[Directions.Down], _directions[Directions.Up]);
-                
-            }
-
-            var output = new[] { direction };
+                (float)directionsKeyMapper.PrimaryDirections,  
+                (float)directionsKeyMapper.SecondaryDirections
+            };
 
             return output;
         }
