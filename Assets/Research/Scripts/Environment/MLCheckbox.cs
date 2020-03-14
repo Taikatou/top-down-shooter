21fbedf4d614da6b40716533dd5390f3a80db571
Taikatou <conorstephens@gmail.com>
2020-03-14 21:10:17 +0000

    update all

---
+++ Assets/Research/Scripts/Environment/MLCheckbox.cs
@@ -0,0 +1,14 @@
+﻿using MoreMountains.TopDownEngine;
+
+namespace Research.Scripts.Environment
+{
+    public class MLCheckbox : CheckPoint
+    {
+        public int playerId;
+        public override void SpawnPlayer(Character player)
+        {
+            base.SpawnPlayer(player);
+            player.PlayerID = "Player" + playerId;
+        }
+    }
+}
