29fed244379a9c5f8dd17f543726df2e27a15dde
Taikatou <conorstephens@gmail.com>
2020-03-14 17:41:25 +0000

    update all

---
+++ Assets/Research/Scripts/TeamDamangeOnTouch.cs
@@ -0,0 +1,44 @@
+﻿using MLAgents.Policies;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace Research.Scripts
+{
+    public class TeamDamangeOnTouch : DamageOnTouch
+    {
+        private bool _isTeam;
+        protected override void Colliding(GameObject collider)
+        {
+            if (Owner)
+            {
+                var owner = Owner.GetComponent<BehaviorParameters>();
+                var character = collider.GetComponent<BehaviorParameters>();
+
+                if (character)
+                {
+                    _isTeam = owner.TeamId == character.TeamId;
+
+                    if (!HealingItem && _isTeam)
+                    {
+                        return;
+                    }
+                }
+            }
+            base.Colliding(collider);
+        }
+
+        protected override void OnCollideWithDamageable(Health health)
+        {
+            base.OnCollideWithDamageable(health);
+            if (!_isTeam)
+            {
+                // we apply the damage to the thing we've collided with
+                _colliderHealth.Damage(DamageCaused, gameObject, InvincibilityDuration, InvincibilityDuration);
+                if (DamageTakenEveryTime + DamageTakenDamageable > 0)
+                {
+                    SelfDamage(DamageTakenEveryTime + DamageTakenDamageable);
+                }
+            }
+        }
+    }
+}
