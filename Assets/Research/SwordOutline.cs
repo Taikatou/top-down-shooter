b80f5345b9cdd88625b2f614b660915c54c2d8c8
Taikatou <conorstephens@gmail.com>
2020-03-24 21:49:38 +0000

    update all

---
+++ Assets/Research/SwordOutline.cs
@@ -0,0 +1,24 @@
+﻿using Research.Scripts.Characters;
+using UnityEngine;
+
+namespace Research
+{
+    public class SwordOutline : MonoBehaviour
+    {
+        public SpriteOutline outline;
+
+        private bool ParentBlue
+        {
+            get
+            {
+                var owner = GetComponentInParent<TopDownMeleeWeapon>()?.Owner;
+                return owner != null && owner.GetComponentInChildren<SpriteOutline>().IsBlue;
+            }
+        }
+
+        private void Start()
+        {
+            outline.IsBlue = ParentBlue;
+        }
+    }
+}
