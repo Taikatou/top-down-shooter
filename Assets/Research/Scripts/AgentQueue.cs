e721b1164d44628ba83dfe9dfd089abf530fd49d
Taikatou <conorstephens@gmail.com>
2020-03-25 21:13:44 +0000

    update all

---
+++ Assets/Research/Scripts/AgentQueue.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace Research.Scripts
+{
+    public class AgentQueue : MonoBehaviour
+    {
+        public Character[] mlCharacters;
+
+        public Character[] priorMlCharacters;
+
+        public AvailableCharacters availableCharacters;
+        
+        public AvailableCharacters availablePriorCharacters;
+
+        public int numAgents = 4;
+
+        private void Start()
+        {
+            availableCharacters = new AvailableCharacters();
+            availablePriorCharacters = new AvailableCharacters();
+
+            for (var i = 0; i < numAgents; i++)
+            {
+                SpawnCharacters(mlCharacters, availableCharacters);
+                SpawnCharacters(priorMlCharacters, availablePriorCharacters);   
+            }
+        }
+
+        private void SpawnCharacters(Character[] characters, AvailableCharacters available)
+        {
+            foreach (var playerPrefab in characters)
+            {
+                var newPlayer = Instantiate(playerPrefab, gameObject.transform.position, Quaternion.identity);
+                available.Add(newPlayer);
+                newPlayer.gameObject.SetActive(false);
+            }
+        }
+
+        public void ReturnCharacters(List<Character> players)
+        {
+            foreach (var player in players)
+            {
+                player.Reset();
+                availableCharacters.ReturnCharacter(player);
+                availablePriorCharacters.ReturnCharacter(player);
+                player.gameObject.SetActive(false);
+            }
+        }
+
+        public Character PopRandomMlCharacter()
+        {
+            return availableCharacters.PopRandomCharacter();
+        }
+        
+        public Character PopRandomPriorMlCharacter()
+        {
+            return availablePriorCharacters.PopRandomCharacter();
+        }
+    }
+}
---
+++ Assets/Research/Scripts/AvailableCharacters.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+
+namespace Research.Scripts
+{
+    public class AvailableCharacters
+    {
+        private readonly List<Character> _availableMlCharacters;
+
+        private readonly List<Character> _allCharacters;
+        
+        private System.Random _random;
+
+        public AvailableCharacters()
+        {
+            _random = new System.Random();
+            _availableMlCharacters = new List<Character>();
+            _allCharacters = new List<Character>();
+        }
+
+        public void Add(Character newCharacter)
+        {
+            _availableMlCharacters.Add(newCharacter);
+            _allCharacters.Add(newCharacter);
+        }
+        
+        public Character PopRandomCharacter()
+        {
+            var index = _random.Next(0, _availableMlCharacters.Count);
+            var character = _availableMlCharacters[index];
+            _availableMlCharacters.Remove(character);
+            
+            character.gameObject.SetActive(true);
+            return character;
+        }
+
+        public void ReturnCharacter(Character character)
+        {
+            if (_allCharacters.Contains(character))
+            {
+                _availableMlCharacters.Add(character);
+            }
+        }
+    }
+}
