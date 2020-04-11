c260478d02965dd6b5af2e43d6b78f3b0f7c3862
Taikatou <conorstephens@gmail.com>
2020-04-11 20:36:13 +0100

    update all

---
+++ Assets/Research/LevelDesign/Scripts/GetSpawnProcedural.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using Research.CharacterDesign.Scripts.Environment;
+using Research.CharacterDesign.Scripts.SpawnPoints;
+using UnityEngine;
+
+namespace Research.LevelDesign.Scripts
+{
+    public class GetSpawnProcedural : IGetSpawnPoints
+    {
+
+        public override List<CheckPoint> Points => new List<CheckPoint>(GetComponentsInChildren<MLCheckbox>());
+    }
+}
