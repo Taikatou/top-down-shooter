5a82c27860445669621cdec180072cd0cfdd37c5
Taikatou <conorstephens@gmail.com>
2020-07-18 11:51:32 +0100

    update screenshot

---
+++ Assets/Research/UI/ScreenShot.cs
@@ -0,0 +1,14 @@
+﻿using Research.LevelDesign.NuclearThrone;
+using UnityEngine;
+
+namespace Research.UI
+{
+    public class ScreenShot : MonoBehaviour
+    {
+        public NuclearThroneLevelGenerator generator;
+        void OnMouseDown()
+        {
+            ScreenCapture.CaptureScreenshot(generator.MapSeed.ToString());
+        }
+    }
+}
